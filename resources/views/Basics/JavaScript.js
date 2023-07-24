1e145a5329bdab4e09fc807e5ea9d270153d0a88
moshiur1412 <moshiur1412@gmail.com>
2023-07-24 09:40:16 +0600

    Alhamduillah for starting batch102

---
+++ resources/views/Basics/JavaScript.js
@@ -0,0 +1,22 @@
+const $arrayString = ["A","B","C","D","E","F"];
+                     [ 0,  1,    2, 3,  4,  5] = 6
+const $arrayNumber = [1,2,3,4,5];
+
+let $string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+// console.log(typeof $arrayString);
+
+$arrayString[0] = "G"; // 
+console.log($arrayString[0]); // G
+console.log($arrayString); // [G,B,C,D,E,F]
+
+console.log($arrayString.toString()); // G,B,C,D,E,F
+console.log($arrayString[$arrayString.length]); // undefined
+
+// Last Value Detect = value 
+console.log($arrayString[$arrayString.length-1]); // F
+
+$arrayString[$arrayString.length-1] = "Last";
+console.log($arrayString); // [ 'G', 'B', 'C', 'D', 'E', 'Last' ]
+// console.log(($string.length));
