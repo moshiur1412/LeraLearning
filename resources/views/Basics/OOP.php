6be8fd9b28004c64c71f97045bcfaebb499afb28
moshiur1412 <moshiur1412@gmail.com>
2023-08-07 09:59:46 +0600

    invoke method implemented

--- resources/views/Basics/OOP.php
+++ resources/views/Basics/OOP.php
@@ -7,51 +7,17 @@ class Car {
     private $size;
     protected $color;
 
-
-    // Methods -->
-      protected function addSize($size){
-        return $size *2;
+    public function __invoke(...$a){
+        return $a;
     }
-
-    public function setSize($givenSize){
-        return $this->size = $givenSize;
-    }
-    public function getSize(){
-        return $this->addSize($this->size);
-    }
-
-    public function setName($name){
-        return $this->name = $name;
-    }
-    public function getName(){
-        return strtoupper($this->name);
-    }
-
-    public function setColor($color){
-        return $this->color = $color;
-    }
-
-    public function getColor(){
-        return $this->color;
-    }
-
-
-   public function printAll(){
-    echo "Car Name: ", $this->getName(), "\nCar Color: ", $this->getColor(), "\nCar Size: ", $this->getSize();
-   }
 
   
 
 }
 
-// object declearation -->
 
 $obj = new Car();
-$obj->setName("Valvo");
-$obj->setColor("Red");
-$obj->setSize(18);
-
-$obj->printAll();
+print_r($obj(4, 5,4,5,6,7,8,4));
 
 
 
